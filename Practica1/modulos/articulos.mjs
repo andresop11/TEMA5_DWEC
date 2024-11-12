56c62844ace28b06df164a62f24aa5bc204ff919
andresop11 <149503566+andresop11@users.noreply.github.com>
2024-11-12 20:32:01 +0100

    Add files via upload

---
+++ Practica1/modulos/articulos.mjs
@@ -0,0 +1,9 @@
+// modulos/articulos.mjs
+export const arrArticulos = [
+  { codigo: 1, nombre: "Portatil", precio: 1200, descripcion: "Portatil de alta gama" },
+  { codigo: 2, nombre: "Movil", precio: 800, descripcion: "Movil con excelente cámara" },
+  { codigo: 3, nombre: "Tablet", precio: 400, descripcion: "Tablet compacta y potente" },
+  { codigo: 4, nombre: "Cascos", precio: 150, descripcion: "Cascos con cancelación de ruido" }
+];
+
+
