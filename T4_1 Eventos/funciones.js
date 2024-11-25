0a3763d2fbbfd17ee0d92ddc3656ad37827398ae
andresop11 <149503566+andresop11@users.noreply.github.com>
2024-11-25 19:20:51 +0100

    Add files via upload

---
+++ T4_1 Eventos/funciones.js
@@ -0,0 +1,9 @@
+        // Captura el evento click en el botón
+            document.addEventListener("click", function () {
+             alert("Esto es un alert");
+            });
+
+    //Ejercicio 2
+
+ 
+
